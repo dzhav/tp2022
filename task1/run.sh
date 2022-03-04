82209460d6a7242d6a335a3235437195ffc7c9ff
dzhavid <fataliev.ds@phystech.edu>
2022-03-04 19:35:37 +0300

    lole

---
+++ task1/run.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+while [ $# != 0 ]
+  do
+    if [ $1 == "--input_folder" ]
+      then
+        input_folder=$2;
+      else
+        if [ $1 == "--extension" ]
+          then
+            extension=$2;
+          else
+            if [ $1 == "--backup_folder" ]
+                      then
+                        backup_folder=$2;
+                      else
+                        if [ $1 == "--backup_archive_name" ]
+                          then
+                            backup_archive_name=$2;
+                          fi
+                      fi
+          fi
+      fi
+      shift
+      shift
+  done
+
+mkdir "$backup_folder"
+
+for val in $(find $input_folder -type f -name "*.$extension")
+  do
+    cp --backup=numbered "$val" "$backup_folder/"
+  done
+
+tar -czf "$backup_archive_name" "$backup_folder"
+
+
+echo "done"
+
