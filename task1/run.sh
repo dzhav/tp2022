ae08478bc63af7846f5ec6163b80f052e006374d
dzhavid <fataliev.ds@phystech.edu>
2022-03-06 23:35:26 +0300

    refresh PR

--- task1/run.sh
+++ task1/run.sh
@@ -5,24 +5,20 @@ while [ $# != 0 ]
     if [ $1 == "--input_folder" ]
       then
         input_folder=$2;
-      else
-        if [ $1 == "--extension" ]
-          then
-            extension=$2;
-          else
-            if [ $1 == "--backup_folder" ]
-                      then
-                        backup_folder=$2;
-                      else
-                        if [ $1 == "--backup_archive_name" ]
-                          then
-                            backup_archive_name=$2;
-                          fi
-                      fi
-          fi
-      fi
-      shift
-      shift
+    fi
+    if [ $1 == "--extension" ]
+      then
+        extension=$2;
+    fi
+    if [ $1 == "--backup_folder" ]
+      then
+        backup_folder=$2;
+    fi
+    if [ $1 == "--backup_archive_name" ]
+      then
+        backup_archive_name=$2;
+    fi
+    shift 2
   done
 
 mkdir "$backup_folder"
@@ -33,7 +29,6 @@ for val in $(find $input_folder -type f -name "*.$extension")
   done
 
 tar -czf "$backup_archive_name" "$backup_folder"
-
 
 echo "done"
 
