e1fe1db1761e230349e0e51f00f737b0461a4c72
dzhavid <fataliev.ds@phystech.edu>
2022-03-14 03:11:11 +0300

    task2 added

---
+++ task2/B/lib.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+#include <iostream>
+
+int getCount(){
+	int b = 2 * 2;
+	return b;
+}
+
+void brawlStars(){
+	return;
+}
---
+++ task2/C/main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <iostream>
+
+int main(){
+	int b = getCount();
+	brawlStars();
+	std::cout << b;
+}
