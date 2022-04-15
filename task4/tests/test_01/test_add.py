7c718ed4957fc21b64b915f0ab713068fd3bb073
dzhavid <fataliev.ds@phystech.edu>
2022-04-15 19:54:31 +0300

    task4

--- task4/tests/test_01/test_add.py
+++ task4/tests/test_01/test_add.py
@@ -2,9 +2,8 @@ from simple_library_01.functions import add
 
 
 def test_add():
-	assert 4 == add(2,2)
-	assert 25 == add(10,15)
-	assert 5 == add(1,4)
-	assert 2021 == add(1001, 1020)
-	assert 2500 == add(150, 2350)
-
+        assert 4 == add(2,2)
+        assert 25 == add(10,15)
+        assert 5 == add(1,4)
+        assert 2021 == add(1001, 1020)
+        assert 2500 == add(150, 2350)
