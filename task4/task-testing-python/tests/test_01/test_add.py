3eb324a3900765a88527238eeb2597c0d5b0c7aa
dzhavid <fataliev.ds@phystech.edu>
2022-04-15 19:17:30 +0300

    task4

---
+++ task4/task-testing-python/tests/test_01/test_add.py
@@ -0,0 +1,10 @@
+from simple_library_01.functions import add
+
+
+def test_add():
+	assert 4 == add(2,2)
+	assert 25 == add(10,15)
+	assert 5 == add(1,4)
+	assert 2021 == add(1001, 1020)
+	assert 2500 == add(150, 2350)
+
